4b30b5ecbf3b057204082cf6b6cbf5c940d681dd
wittea91 <wittea91@my.sunyulster.edu>
2018-05-07 22:28:26 -0400

    Pong
    Pong

---
+++ Assets/_Scripts/SideWalls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour {
+
+
+	void OnTriggerEnter2D (Collider2D hitInfo) {
+		if (hitInfo.name == "Ball")
+		{
+			string wallName = transform.name;
+			GameManager.Score(wallName);
+			hitInfo.gameObject.SendMessage("RestartGame", 1.0f, SendMessageOptions.RequireReceiver);
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
